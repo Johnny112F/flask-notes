69dbfb693c0043f12ffda4debb9cb56860908ced
johnathanraiss <johnathan.raiss@gmail.com>
2021-03-16 14:32:24 -0400

    User class and connect_db function made

---
+++ models.py
@@ -0,0 +1,28 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt 
+
+bcrypt = Bcrypt()
+db = SQLAlchemy()
+
+
+class User(db.Model):
+    """Site user"""
+
+    __tablename__ = "users"
+
+    username = db.Column(db.String(20),
+                         nullable=False,
+                         unique=True,
+                         primary_key=True,
+                         )
+    password = db.Column(db.Text, nullable=False)
+    email = db.Column(db.String(50), nullable=False)
+    first_name = db.Column(db.String(30), nullable=False)
+    Last_name = db.Column(db.String(30), nullable=False)
+
+
+def connect_db(app):
+    """Connect database to Flask app."""
+
+    db.app = app
+    db.init_app(app)
